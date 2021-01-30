20ee0052289ff7ece4722c5a93390382b3eb2b4a
vicky7274 <vigneshwaran2747@gmail.com>
2021-01-30 13:27:45 +0530

    removed and modified errors

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express=require('express');
-const app=express();
-const morgan=require('morgan');
-const bodypraser=require('body-parser');
-const mongoose=require('mongoose');
-const emproutes=require('./api/routes/employee');
-mongoose.connect('mongodb://localhost:27017/shop', {useNewUrlParser: true,  useUnifiedTopology: true });
-console.log('databases created');
-app.use('/employee',emproutes);
-app.use(morgan('dev'));
-app.use(bodypraser.urlencoded({extended:false}));
-app.use(bodypraser.json()); 
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodypraser = require("body-parser");
+const mongoose = require("mongoose");
+const emproutes = require("./api/routes/employee");
+mongoose.connect("mongodb://localhost:27017/shop", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// console.log("databases created");
+app.use(bodypraser.urlencoded({ extended: false }));
+app.use(bodypraser.json());
 
-module.exports=app;
+app.use(morgan("dev"));
+app.use("/employee", emproutes);
+
+module.exports = app;
